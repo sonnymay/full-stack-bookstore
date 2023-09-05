9ef2045c06aad32271e9c8266a6e56862a0983af
Santipap May <santipapmay@santipaps-air.lan>
2023-09-05 00:29:22 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # full-stack-bookstore
 # full-stack-bookstore
+# fullstack-bookstore-frontend
--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://may:may@cluster0.02iy63a.mongodb.net/?retryWrites=true&w=majority';
